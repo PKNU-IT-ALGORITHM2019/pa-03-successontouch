27fe02c2b567118bfad49831ab774d59f0041111
Min-Gyu Kim <successontouch@gmail.com>
2019-03-30 03:20:21 +0900

    third2

--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -4,7 +4,7 @@
 #include <math.h>
 #pragma warning(disable:4996)
 
-#define MAX 1000000
+#define MAX 100000
 clock_t end, start;
 
 int man[MAX];
@@ -61,7 +61,6 @@ int mid_partition(int p, int r) {
 			man[i] = man[j];
 			man[j] = tmp;
 		}
-		
 	}
 	tmp = man[i + 1];
 	man[i + 1] = man[r];
@@ -71,7 +70,6 @@ int mid_partition(int p, int r) {
 }
 
 void mid_quickSort(int p, int r) {
-
 	if (p < r) {
 		int q = mid_partition(p, r);
 		mid_quickSort(p, q - 1);
@@ -79,30 +77,32 @@ void mid_quickSort(int p, int r) {
 	}
 }
 int partition(int p, int r) {
+
 	int x = man[r];
 	int i = p - 1;
+	int tmp;
 	for (int j = p; j < r; j++) {
 		if (man[j] <= x) {
+			
 			i = i + 1;
-			int tmp = man[i];
+			tmp = man[i];
 			man[i] = man[j];
 			man[j] = tmp;
 		}
-
 	}
-	int tmp = man[i + 1];
+	
+	tmp = man[i + 1];
 	man[i + 1] = man[r];
 	man[r] = tmp;
 	return i + 1;
-
 }
 void quickSort(int p, int r) {
+	
 	if (p < r) {
 		int q = partition(p, r);
 		quickSort(p, q - 1);
 		quickSort(q + 1, r);
 	}
-
 }
 void merge(int p, int q, int r) {
 	int i = p, j = q + 1, k = p;
@@ -182,7 +182,7 @@ void rand_create(int N) {
 	//랜덤
 	
 	for (int i = 0; i < N; i++) {
-		man[i] = rand() % N;
+		man[i] = rand() % N+1;
 	}
 	
 
@@ -194,70 +194,99 @@ void reverse_create(int N) {
 	for (int i = 0; i < N; i++) {
 		man[i] = N-i;
 	}
-
-
-
+}
+int fun(int choice, int choice2, int num)
+{
+	double time = 0;
+	for (int i = 0; i < 10; i++) {
+		switch (choice)
+		{
+			case 1: {
+				rand_create(num);
+				break;
+			}
+			case 2: {
+				reverse_create(num);
+				break;
+			}
+				
+		}
+		start = clock();
+		switch (choice2) {
+			case 1: {
+				bubble(num);
+				break;
+			}
+			case 2: {
+				selection(num);
+				break;
+			}
+			case 3: {
+				insertion(num);
+				break;
+			}
+			case 4: {
+				mergeSort(0, num-1);
+				break;
+			}
+			case 5: {
+				quickSort(0, num-1);
+				break;
+			}
+			case 6: {
+				mid_quickSort(0, num-1);
+				break;
+			}
+			case 7: {
+				rand_quickSort(0, num-1);
+				break;
+			}
+		}
+		end = clock();
+		time += end - start;
+	}
+	return time / 10.0;
 }
 void main()
 {
 	clock_t start, end;
 	srand((unsigned)time(NULL));
-	start = clock();
+	
 	int num;
-	printf("랜덤 1 리버스 2\n");
-	int choice;
-	scanf("%d", &choice);
-	printf("num ?");
-	scanf("%d", &num);
-	switch (choice)
-	{
-	case 1:{
-			rand_create(num);
-			break;
-		}
-	case 2:{
-			reverse_create(num);
-			break;
-		}
-	}
-	int choice2;
-	printf("버블 1 선택 2 삽입 3 합병 4 빠른 5 빠른 6 빠른 7\n");
-	scanf("%d", &choice2);
-	switch (choice2) {
-	case 1: {
-		bubble(num);
-		break;
-	}
-	case 2: {
-		selection(num);
-		break;
-	}
-	case 3: {
-		insertion(num);
-		break;
-	}
-	case 4: {
-		mergeSort(0,num);
-		break;
-	}
-	case 5: {
-		quickSort(0, num);
-		break;
-	}
-	case 6: {
-		mid_quickSort(0, num);
-		break;
-	}
-	case 7: {
-		rand_quickSort(0, num);
-		break;
-	}
-
-	}
-	print(num);
-	end = clock();
+	
+			
+			printf("Random1000\tReverse1000\tRandom10000\tReverse10000\tRandom100000\tReverse100000\n");
 
 
+			for (int i = 5; i <= 7; i++) {
+				
+				
+				int time = fun(1, i, 1000);
+				printf("%.3lf초\t", time / 100.0);
+
+				time = fun(2, i, 1000);
+				printf("\t%.3lf초\t", time / 100.0);
+
+				
+				time = fun(1, i, 10000);
+				printf("\t%.3lf초\t", time / 100.0);
+
+				time = fun(2, i, 10000);
+
+				printf("\t%.3lf초\t", time / 100.0);
+
+				time = fun(1, i, 100000);
+
+				printf("\t%.3lf초\t", time / 100.0);
+
+				time = fun(2, i, 100000);
+
+				printf("\t%.3lf초\t", time / 100.0);
+
+				printf("\n");
+			}
+			
+		
 	getchar();
 	getchar();
 }
